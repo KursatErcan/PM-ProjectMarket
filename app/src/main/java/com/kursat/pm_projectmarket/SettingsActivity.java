bcb9a03b93d53e922449356e098f7b4079ea1fe1
kursat ercan <348338@ogr.ktu.edu.tr>
2021-04-13 16:14:47 +0300

    13.04.2021

---
+++ app/src/main/java/com/kursat/pm_projectmarket/SettingsActivity.java
@@ -0,0 +1,78 @@
+package com.kursat.pm_projectmarket;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Switch;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.kursat.pm_projectmarket.Fragment.FeedFragment;
+
+public class SettingsActivity extends AppCompatActivity {
+    SharedPreferences mode;
+    SharedPreferences.Editor editor;
+    boolean isNightModeOn;
+    TextView logout;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+
+        logout = (TextView) findViewById(R.id.logout);
+
+        //function for enabling dark mode
+        //mode = getActivity().getSharedPreferences("modePref",Context.MODE_PRIVATE);
+        mode = getSharedPreferences(SettingsActivity.this.getPackageName(), MODE_PRIVATE);
+        editor = mode.edit();
+        isNightModeOn = mode.getBoolean("NightMode",false);
+
+        if(isNightModeOn){
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+        }else{
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+        }
+
+        @SuppressLint("UseSwitchCompatOrMaterialCode") Switch modeSwitch = findViewById(R.id.darkModeSwitch);
+        modeSwitch.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            if (isChecked){
+                if(isNightModeOn){
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO); // BUYUK İHTİMAL HATA BURADA
+                    editor.putBoolean("NightMode",false);
+                }else{
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                    editor.putBoolean("NightMode",true);
+                }
+                editor.apply();
+            }
+        });
+
+        logout.setOnClickListener((v) -> {
+            new AlertDialog.Builder(this)
+                    .setMessage("Are you sure you want to Logout?")
+                    .setCancelable(false)
+                    .setPositiveButton("Yes",(dialog, which) ->{
+                        FirebaseAuth.getInstance().signOut();
+                        Intent intent = new Intent(SettingsActivity.this, IntroActivity.class);
+                        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                        startActivity(intent);
+                    })
+                    .setNegativeButton("No", null)
+                    .show();
+        });
+
+    }
+
+    private void editProfileClicked(View view){
+        //getFragmentManager().beginTransaction().replace(R.id.mainFragment,new FeedFragment()).commit();
+
+    }
+}
