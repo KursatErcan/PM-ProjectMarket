19af76e67b3697433e6bc778f2fa1e8b305ab15f
Burak <340828@ogr.ktu.edu.tr>
2021-06-01 01:12:02 +0300

    Profile work in progress

---
+++ app/src/main/java/com/kursat/pm_projectmarket/Model/Profile.java
@@ -0,0 +1,13 @@
+package com.kursat.pm_projectmarket.Model;
+
+import java.util.ArrayList;
+
+public class Profile {
+    private String userName;
+    private String userId;
+    private String postImageUrl;
+    private ArrayList<String> userComments;
+
+    
+
+}
