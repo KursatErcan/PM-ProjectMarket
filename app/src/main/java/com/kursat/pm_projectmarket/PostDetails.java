bbbd0e77b8d05f0b7b9915bb0b3973abc947d593
Burak <340828@ogr.ktu.edu.tr>
2021-06-01 01:11:19 +0300

    Profile work in progress

--- app/src/main/java/com/kursat/pm_projectmarket/PostDetails.java
+++ app/src/main/java/com/kursat/pm_projectmarket/PostDetails.java
@@ -1,17 +1,86 @@
 package com.kursat.pm_projectmarket;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+import com.kursat.pm_projectmarket.Model.MessageSend;
+import com.kursat.pm_projectmarket.Model.Post;
+
+import java.util.ArrayList;
+
+import static androidx.constraintlayout.motion.utils.Oscillator.TAG;
 
 public class PostDetails extends AppCompatActivity {
     String token;
+    private FirebaseFirestore db;
+    FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+    ArrayList<Post> ppost;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post_details);
         token=getIntent().getStringExtra("token");
+        db = FirebaseFirestore.getInstance();
+        ppost= new ArrayList<>();
+        DocumentReference docRef = db.collection("Posts").document(token);
+        docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot document = task.getResult();
+                    if (document.exists()) {
+                        Post post = document.toObject(Post.class);
+
+                        ppost.add(new Post(post.getUserId(),post.getUserName(),post.getPrice(),post.getTitle(),post.getPostImageUrl(),post.getUserId()));
+
+                        //Log.d(TAG, "DocumentSnapshot data: " + document.getData());
+                    } else {
+                        Log.d(TAG, "No such document");
+                    }
+                } else {
+                    Log.d(TAG, "get failed with ", task.getException());
+                }
+            }
+        });
+        /*db.collection("Posts/"+token)
+                .addSnapshotListener(new EventListener<QuerySnapshot>() {
+                    @Override
+                    public void onEvent(@Nullable QuerySnapshot value, @Nullable FirebaseFirestoreException e) {
+                        if (e != null) {
+                            Log.w(TAG, "Listen failed.", e);
+                            return;
+                        }
+
+                        ppost.clear();
+                        for (QueryDocumentSnapshot doc1 : value) {
+                            Post post = doc1.toObject(Post.class);
+                            //tring userId,String userName, String price, String title, String postImageUrl,String token
+                            ppost.add(new Post(post.getUserId(),post.getUserName(),post.getPrice(),post.getTitle(),post.getPostImageUrl(),post.getUserId()));
+
+
+                        }
+
+                    }
+
+                });*/
+
+
 
     }
 }
