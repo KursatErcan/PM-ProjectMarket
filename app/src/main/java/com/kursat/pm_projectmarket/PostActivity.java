4e9afa08461dff7f96bb4f0172bee4ae7ea25299
kursat ercan <348338@ogr.ktu.edu.tr>
2021-06-04 03:55:28 +0300

    post details fragment eklendi, küçük değişiklikler yapıldı

--- app/src/main/java/com/kursat/pm_projectmarket/PostActivity.java
+++ app/src/main/java/com/kursat/pm_projectmarket/PostActivity.java
@@ -1,56 +1,37 @@
 package com.kursat.pm_projectmarket;
 
-import android.Manifest;
-import android.app.ProgressDialog;
+import android.annotation.SuppressLint;
 import android.content.ContentResolver;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
-import android.graphics.ImageDecoder;
 import android.net.Uri;
-import android.os.Build;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.view.View;
 import android.webkit.MimeTypeMap;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.ImageView;
-import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
 
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.Timestamp;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.CollectionReference;
-import com.google.firebase.firestore.FieldValue;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
-import com.squareup.picasso.Picasso;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
-import java.util.Objects;
-import java.util.UUID;
 
 public class PostActivity extends AppCompatActivity {
     private ImageView postImage;
@@ -69,9 +50,10 @@ public class PostActivity extends AppCompatActivity {
     CheckBox chkWriting;
     CheckBox chkProgramming;
     HashMap<String,String> array;
-    Picasso picasso;
-    private Spinner spinner;
-    final String[] categoryId = new String[1];
+
+
+    public PostActivity() {
+    }
 
 
     @Override
@@ -121,11 +103,14 @@ public class PostActivity extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-        if(requestCode==1 && resultCode==RESULT_OK && data.getData() != null){
-            imageData=data.getData();
+        if(requestCode == 1 && resultCode == RESULT_OK) {
+            assert data != null;
+            if (data.getData() != null) {
+                imageData = data.getData();
 
-            postImage.setImageURI(imageData);
+                postImage.setImageURI(imageData);
 
+            }
         }
     }
 
@@ -146,39 +131,31 @@ public class PostActivity extends AppCompatActivity {
                 bmp.compress(Bitmap.CompressFormat.JPEG, 25, baos);
                 byte[] data = baos.toByteArray();
                 UploadTask uploadTask2 = fileReference.putBytes(data);
-                uploadTask2.addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
-                    @Override
-                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
-                        fileReference.getDownloadUrl().addOnSuccessListener(uri -> {
-                            String downloadUrl = uri.toString();
-                            HashMap<String,Object> postData = new HashMap<>();
-                            postData.put("date",new Timestamp(new Date()));
-                            postData.put("postImageUrl",downloadUrl.toString());
-                            postData.put("price",priceText.getText().toString());
-                            postData.put("title",title.getText().toString());
-                            postData.put("userId",user.getUid().toString());
-                            postData.put("userName",userName);
-                            postData.put("postCategory",array);
-                            //add the data into the Posts
-                            cfr=db.collection("Posts");
-                            cfr.add(postData);
+                uploadTask2.addOnSuccessListener(taskSnapshot -> {
+                    fileReference.getDownloadUrl().addOnSuccessListener(uri -> {
+                        String downloadUrl = uri.toString();
+                        HashMap<String,Object> postData = new HashMap<>();
+                        postData.put("date",new Timestamp(new Date()));
+                        postData.put("postImageUrl", downloadUrl);
+                        postData.put("price",priceText.getText().toString());
+                        postData.put("title",title.getText().toString());
+                        postData.put("userId", user.getUid());
+                        postData.put("userName",userName);
+                        postData.put("postCategory",array);
+                        //add the data into the Posts
+                        cfr=db.collection("Posts");
+                        cfr.add(postData);
 
 
 
 
-                        });
+                    });
 
-                        Toast.makeText(PostActivity.this, "Your post is successfully posted!", Toast.LENGTH_LONG).show();
-                        startActivity(new Intent(PostActivity.this, MainActivity.class));
-                        finish();
+                    Toast.makeText(PostActivity.this, "Your post is successfully posted!", Toast.LENGTH_LONG).show();
+                    startActivity(new Intent(PostActivity.this, MainActivity.class));
+                    finish();
 
-                    }
-                }).addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        Toast.makeText(PostActivity.this, "Upload Failed -> " + e, Toast.LENGTH_LONG).show();
-                    }
-                });
+                }).addOnFailureListener(e -> Toast.makeText(PostActivity.this, "Upload Failed -> " + e, Toast.LENGTH_LONG).show());
 
 
             } catch (IOException e) {
@@ -191,6 +168,7 @@ public class PostActivity extends AppCompatActivity {
         }
     }
 
+    @SuppressLint("NonConstantResourceId")
     public HashMap<String,String> onCheckboxClicked(View view){
         //selected list of category
         boolean checked = ((CheckBox) view).isChecked();
